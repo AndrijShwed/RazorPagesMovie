61d015b3f7f9585bdadd7f0bb9fda14c290fddea
AndrijShwed <a_shwed@ukr.net>
2023-06-06 17:01:05 +0300

    First commit

---
+++ RazorPagesMovie/Pages/Shared/_ValidationScriptsPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages.Shared
+{
+    public class _ValidationScriptsPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
